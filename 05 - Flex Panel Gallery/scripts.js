590b5773f054ed8b584c758049be6c73f371295c
Giovani Aguirre <aguirregio19@gmail.com>
2018-01-23 17:55:40 -0800

    Adding mouse over effect for panels

--- 05 - Flex Panel Gallery/scripts.js
+++ 05 - Flex Panel Gallery/scripts.js
@@ -1,7 +1,25 @@
 const panel = document.querySelectorAll(".panel");
 
 function hover(){
+  // console.log(this.classList);
   this.classList.toggle('open');
 }
 
-panel.forEach(panel => panel.addEventListener('click', hover));
+function openActive(e){
+  // Check what properties are transitioning
+  // console.log(e.propertyName)
+  // Some browsers use flex, some use flex-grow
+  // Cover both bases by using includes('flex')
+  console.log(this.classList);
+  if (e.propertyName.includes('flex')){
+    if (this.classList.contains('open-active')) {
+      this.classList.remove('open-active');
+    } else {
+      this.classList.add('open-active');
+    }
+  }
+}
+
+panel.forEach(panel => panel.addEventListener('mouseenter', hover));
+panel.forEach(panel => panel.addEventListener('mouseleave', hover));
+panel.forEach(panel => panel.addEventListener('transitionend', openActive));
