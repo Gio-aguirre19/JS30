2109f15be60019f55d0082bdf94a743b2dc6296d
Giovani Aguirre <aguirregio19@gmail.com>
2018-02-16 20:26:30 -0800

    Taco checks are now in local storage

--- 15 - LocalStorage/scripts.js
+++ 15 - LocalStorage/scripts.js
@@ -1,5 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
+// Parse seperates strings and makes them objects
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e){
@@ -13,9 +14,19 @@ function addItem(e){
   items.push(item);
   populateList(items, itemsList);
   // Adding items to local storage in 'items' json
+  // Stringify combines object and makes a string
   localStorage.setItem('items', JSON.stringify(items));
   // After submit, empties input
   this.reset();
+}
+
+function toggleDone(e){
+  if (!e.target.matches('input')) return; // Skips function unless input is clicked
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
 }
 
 function populateList(plates = [], platesList){
@@ -32,5 +43,6 @@ function populateList(plates = [], platesList){
 }
 
 addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
 
 populateList(items, itemsList);
