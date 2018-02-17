25048fbfb57a57b9238429384325983bc961db73
Giovani Aguirre <aguirregio19@gmail.com>
2018-02-16 19:32:02 -0800

    Adding form functionality. Grabbing input value after submit without refreshing page

--- 15 - LocalStorage/scripts.js
+++ 15 - LocalStorage/scripts.js
@@ -1,3 +1,18 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(e){
+  // Prevents from refreshing page (default)
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  }
+  items.push(item);
+  // After submit, empties input
+  this.reset();
+}
+
+addItems.addEventListener('submit', addItem);
