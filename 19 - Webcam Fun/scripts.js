9bbd6207b0ff0a9760c3951f92d47c91cb389c54
Giovani Aguirre <aguirregio19@gmail.com>
2018-03-04 12:58:58 -0800

    You can now take photos using the button on top

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -15,4 +15,32 @@ function getVideo(){
     })
 }
 
+function paintCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height)
+  }, 10);
+}
+
+function takePhoto() {
+  // Play Shutter
+  snap.currentTime = 0;
+  snap.play();
+
+  // Takes data out of canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'hansome');
+  link.innerHTML = `<img src="${data}" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
 getVideo();
+
+video.addEventListener('canplay', paintCanvas);
