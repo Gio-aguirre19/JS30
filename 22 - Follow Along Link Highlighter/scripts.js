6220fc38bdc4bf9919a74f949a2a93ba225c16bd
its-gio <me@its-g.io>
2018-06-30 21:07:59 -0700

    (removed some html and give scripts a type on html) and (Added highlight span and the abiliy to get rect from client hover)

--- 22 - Follow Along Link Highlighter/scripts.js
+++ 22 - Follow Along Link Highlighter/scripts.js
@@ -1,1 +1,11 @@
-console.log("this is working");
+const trigger = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+}
+
+trigger.forEach(a => a.addEventListener("mouseenter", highlightLink))
