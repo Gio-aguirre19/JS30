1fcf08cae09424c6175a464f1e30128c2063c5e3
its-gio <me@its-g.io>
2018-06-30 21:25:38 -0700

    Adding comments and finishing hover effect (Fixing when scrolled)

--- 22 - Follow Along Link Highlighter/scripts.js
+++ 22 - Follow Along Link Highlighter/scripts.js
@@ -1,11 +1,26 @@
 const trigger = document.querySelectorAll('a');
+
+// Making Span and appending it to bottom of body
 const highlight = document.createElement('span');
 highlight.classList.add('highlight');
 document.body.append(highlight);
 
 function highlightLink() {
+  // Getting attributes of element that is hovered
   const linkCoords = this.getBoundingClientRect();
-  console.log(linkCoords);
+
+  // Making our own Coordinates
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  }
+  // Styling using new coords object
+  highlight.style.width = `${coords.width}px`
+  highlight.style.height = `${coords.height}px`
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`
 }
 
+// Adding Event Listeners (Mouse Eneter) to all links
 trigger.forEach(a => a.addEventListener("mouseenter", highlightLink))
