43d39595185fd5695e3b7c72dc058277935880fc
its-gio <me@its-g.io>
2018-07-03 16:41:41 -0700

    Adding 'fixed-nav' class to nav element when window reaches top of nav element

--- 24 - Sticky Nav/scripts.js
+++ 24 - Sticky Nav/scripts.js
@@ -1,1 +1,14 @@
-console.log("Mathmatical!");
+const nav = document.querySelector("#main");
+// Gets top of nav element
+const topOfNav = nav.offsetTop;
+
+function fixNav(){
+  // console.log(topOfNav, window.scrollY);
+  if (window.scrollY > topOfNav){
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+window.addEventListener('scroll', fixNav)
