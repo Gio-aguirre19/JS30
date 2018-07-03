af83b950e589f8df073ba9023e6542cd4681e2e0
its-gio <me@its-g.io>
2018-07-03 16:50:36 -0700

    Making transition smooth by adding padding to top or removing it when nav is fixed

--- 24 - Sticky Nav/scripts.js
+++ 24 - Sticky Nav/scripts.js
@@ -5,8 +5,12 @@ const topOfNav = nav.offsetTop;
 function fixNav(){
   // console.log(topOfNav, window.scrollY);
   if (window.scrollY > topOfNav){
+    // Gets height of nav element and adds it to top of body
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
     document.body.classList.add('fixed-nav');
   } else {
+    // Setting Padding top back to zero
+    document.body.style.paddingTop = 0;
     document.body.classList.remove('fixed-nav');
   }
 }
