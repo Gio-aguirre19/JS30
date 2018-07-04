6ff19a6144e5a74cd121420448b1a75446be3d16
its-gio <me@its-g.io>
2018-07-03 17:16:47 -0700

    Learned about stopPropagation and once option for event listening

--- 25 - Event Capture, Propagation, Bubbling and Once/scripts.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/scripts.js
@@ -2,10 +2,14 @@ const divs = document.querySelectorAll('div');
 
 function logText(e) {
   console.log(this.classList.value);
-  // e.stopPropogation(); // stop bubbling
+  e.stopPropagation(); // stop bubbling and only fires active event
 }
 
 divs.forEach(div => div.addEventListener('click', logText));
+// divs.forEach(div => div.addEventListener('click', logText, {
 // Instead of bubbling, it calls events while going to the source of where it was called.
 // instead of 123 it says 321
-// divs.forEach(div => div.addEventListener('click', logText, {capture: true}));
+// capture: true,
+// Only allows event to use function once
+// once: true
+// }));
