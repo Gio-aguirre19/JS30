f515fd26fbb0aba65d57044bb37dcd0d314c4707
its-gio <me@its-g.io>
2018-07-03 17:12:56 -0700

    Learning about bubbling and capture

--- 25 - Event Capture, Propagation, Bubbling and Once/scripts.js
+++ 25 - Event Capture, Propagation, Bubbling and Once/scripts.js
@@ -1,1 +1,11 @@
-console.log("Milk Duds")
+const divs = document.querySelectorAll('div');
+
+function logText(e) {
+  console.log(this.classList.value);
+  // e.stopPropogation(); // stop bubbling
+}
+
+divs.forEach(div => div.addEventListener('click', logText));
+// Instead of bubbling, it calls events while going to the source of where it was called.
+// instead of 123 it says 321
+// divs.forEach(div => div.addEventListener('click', logText, {capture: true}));
