6b66929b00d6011d26d5347a71adbfb3f1d41f60
its-gio <me@its-g.io>
2018-07-06 16:43:13 -0700

    Adding enter and leave functions and getting query selectors of trigger and nav

--- 26 - Stripe Follow Along Nav/scripts.js
+++ 26 - Stripe Follow Along Nav/scripts.js
@@ -1,1 +1,14 @@
-console.log("This be a test!");
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('.dropdownBackground');
+const nav = document.querySelector('#top');
+
+function handleEnter() {
+  console.log("Enter");
+}
+
+function handleLeave() {
+  console.log("Leave");
+}
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
