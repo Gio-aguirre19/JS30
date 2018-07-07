855be209c3894a690a2f2a14bc9de43023567c6a
its-gio <me@its-g.io>
2018-07-06 17:30:43 -0700

    Saftey edge case if trigger enter active is added

--- 26 - Stripe Follow Along Nav/scripts.js
+++ 26 - Stripe Follow Along Nav/scripts.js
@@ -4,7 +4,8 @@ const nav = document.querySelector('.top');
 
 function handleEnter() {
   this.classList.add('trigger-enter');
-  setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+  // Saftey edge case if trigger enter active is added
+  setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
   background.classList.add('open');
 
   const dropdown = this.querySelector('.dropdown');
