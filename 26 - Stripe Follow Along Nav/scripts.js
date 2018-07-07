77f66b1c25af754cc791ed0676178a9ed13180e0
its-gio <me@its-g.io>
2018-07-06 17:18:03 -0700

    Changing size of box to match size of content

--- 26 - Stripe Follow Along Nav/scripts.js
+++ 26 - Stripe Follow Along Nav/scripts.js
@@ -5,10 +5,25 @@ const nav = document.querySelector('.top');
 function handleEnter() {
   this.classList.add('trigger-enter');
   setTimeout(() => this.classList.add('trigger-enter-active'), 150);
+  background.classList.add('open');
+
+  const dropdown = this.querySelector('.dropdown');
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+  }
+
+  // Rememeber to add px to the end of object value
+  background.style.setProperty('height', `${coords.height}px`);
+  background.style.setProperty('width', `${coords.width}px`);
 }
 
 function handleLeave() {
   this.classList.remove('trigger-enter', 'trigger-enter-active');
+  background.classList.remove('open');
 }
 
 triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
