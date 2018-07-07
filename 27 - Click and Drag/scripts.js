79db641a8ae0a90c1b1416f504b19918770e5267
its-gio <me@its-g.io>
2018-07-07 15:53:07 -0700

    Setting up element grabs and event listeners

--- 27 - Click and Drag/scripts.js
+++ 27 - Click and Drag/scripts.js
@@ -1,1 +1,20 @@
-console.log('This be a test!');
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX, scrollLeft;
+
+slider.addEventListener('mousedown', () => {
+  isDown = true;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+slider.addEventListener('mousemove', () => {
+  if(!isDown) return; // Stop from running this function is isDown is not true
+  
+});
