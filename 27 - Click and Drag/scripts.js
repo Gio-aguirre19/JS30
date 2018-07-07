4ba0ee8b9873521365b1597cd0bff1e409c8cbe9
its-gio <me@its-g.io>
2018-07-07 16:01:51 -0700

    Getting value of X coords when clicked

--- 27 - Click and Drag/scripts.js
+++ 27 - Click and Drag/scripts.js
@@ -2,9 +2,14 @@ const slider = document.querySelector('.items');
 let isDown = false;
 let startX, scrollLeft;
 
-slider.addEventListener('mousedown', () => {
+slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
+  // Log (e) to see what events and values are possible
+  // In this case you are logging page X coords
+  // console.log(e.pageX);
+  // Setting click startX to page X coords minus left margin of slider
+  startX = e.pageX - slider.offsetLeft;
 });
 
 slider.addEventListener('mouseleave', () => {
