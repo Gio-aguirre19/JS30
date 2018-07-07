01cc042618d74b4e2b66c45a912d0af25b78da14
its-gio <me@its-g.io>
2018-07-07 15:55:22 -0700

    Adding and removed class of active when mouse clicks, releases, and leaves

--- 27 - Click and Drag/scripts.js
+++ 27 - Click and Drag/scripts.js
@@ -4,17 +4,20 @@ let startX, scrollLeft;
 
 slider.addEventListener('mousedown', () => {
   isDown = true;
+  slider.classList.add('active');
 });
 
 slider.addEventListener('mouseleave', () => {
   isDown = false;
+  slider.classList.remove('active');
 });
 
 slider.addEventListener('mouseup', () => {
   isDown = false;
+  slider.classList.remove('active');
 });
 
 slider.addEventListener('mousemove', () => {
   if(!isDown) return; // Stop from running this function is isDown is not true
-  
+
 });
