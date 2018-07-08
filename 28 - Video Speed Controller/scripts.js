01d055b6a9c9cf7e8cd22a97ff322391441d7801
its-gio <me@its-g.io>
2018-07-08 16:01:30 -0700

    Move event on speedBar now console.logs a percent on how far down the bar the mouse is

--- 28 - Video Speed Controller/scripts.js
+++ 28 - Video Speed Controller/scripts.js
@@ -1,1 +1,17 @@
-console.log("Testing!");
+const speedBar = document.querySelector('.speed-bar');
+const fill = document.querySelector('.speed-fill');
+const video = document.querySelector('.flex');
+
+function videoSpeed(e) {
+  // Getting position of just speedBar top
+  const y = e.pageY - this.offsetTop;
+  // Dividing position of speedBar top by the height of the speedbar
+  const percent = y / this.offsetHeight;
+  // Setting mix and max fill
+  const min = 0.5;
+  const max = 5;
+  const height = Math.round(percent * 100) + '%';
+  console.log(height);
+}
+
+speedBar.addEventListener('mousemove', videoSpeed);
