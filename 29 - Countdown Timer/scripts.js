15767e4db29e5210ae7970c55d93a5e736651e9c
its-gio <me@its-g.io>
2018-07-09 18:36:50 -0700

    Displaying the first number when entered in

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -3,12 +3,18 @@ function timer(secs) {
   const now = Date.now();
   // now is in milliseconds
   const then = now + (secs * 1000);
+  displayTimeLeft(secs);
+
   countdown = setInterval(() => {
     const secsLeft = Math.round((then - Date.now()) / 1000);
     if (secsLeft < 0) {
       stopInterval(countdown);
       return;
     }
-    console.log(secsLeft);
+    displayTimeLeft(secs);
   }, 1000)
+}
+
+function displayTimeLeft(secs) {
+  
 }
