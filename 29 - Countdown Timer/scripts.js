a43282df7639c95ba3829a98ad50e7e84d7bb1cb
its-gio <me@its-g.io>
2018-07-09 18:28:48 -0700

    Made timer count down

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,3 +1,9 @@
-function timer() {
-  
+function timer(secs) {
+  const now = Date.now();
+  // now is in milliseconds
+  const then = now + (secs * 1000);
+  setInterval(() => {
+    const secsLeft = Math.round((then - Date.now()) / 1000);
+    console.log(secsLeft);
+  }, 1000)
 }
