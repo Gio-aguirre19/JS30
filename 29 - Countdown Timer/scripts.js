e802c7e987f99aa529d5303bccd88363c5c27c3d
its-gio <me@its-g.io>
2018-07-09 18:33:41 -0700

    Stoping coundown when lower then 0

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,9 +1,14 @@
+let countdown;
 function timer(secs) {
   const now = Date.now();
   // now is in milliseconds
   const then = now + (secs * 1000);
-  setInterval(() => {
+  countdown = setInterval(() => {
     const secsLeft = Math.round((then - Date.now()) / 1000);
+    if (secsLeft < 0) {
+      stopInterval(countdown);
+      return;
+    }
     console.log(secsLeft);
   }, 1000)
 }
