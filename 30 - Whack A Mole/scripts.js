fe4cf84bb0e6914f2cc68284bbdff8ca580647fd
its-gio <me@its-g.io>
2018-07-13 18:18:24 -0700

    Making random number generator for time using min and max

--- 30 - Whack A Mole/scripts.js
+++ 30 - Whack A Mole/scripts.js
@@ -1,3 +1,12 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelector('.mole');
+
+function ranTime(min, max) {
+  // Plus min so max so min is min and max is max
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes){
+  console.log(holes.length);
+}
