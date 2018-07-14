62807c9b38d5465c64bff4fe3d994ea997c4149f
its-gio <me@its-g.io>
2018-07-14 13:20:24 -0700

    Start game button now works and can be played for 10 seconds (Keep score functionality needed)

--- 30 - Whack A Mole/scripts.js
+++ 30 - Whack A Mole/scripts.js
@@ -35,3 +35,10 @@ function peep() {
     if (!timeUp) peep();
   }, time);
 }
+
+function startGame() {
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  peep();
+  setTimeout(() => timeUp = true, 10000)
+}
