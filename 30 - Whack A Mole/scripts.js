88d471dce3ca9676ed6dfdbc64c71b919aca9ae9
its-gio <me@its-g.io>
2018-07-14 13:12:52 -0700

    Function peep now shows and removes the mole when called

--- 30 - Whack A Mole/scripts.js
+++ 30 - Whack A Mole/scripts.js
@@ -27,5 +27,8 @@ function randomHole(holes){
 function peep() {
   const time = randomTime(200, 1000);
   const hole = randomHole(holes);
-  console.log(time, hole)
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+  }, time);
 }
